7d4df39dcb6c12d9d6822f7a9b36fc04d9933695
Pavitacode <104275791+Pavitacode@users.noreply.github.com>
2023-05-23 04:37:02 -0500

    test 6

--- app.js
+++ app.js
@@ -73,43 +73,7 @@ io.on('connection', (socket) => {
   });
 
 
- socket.on('message', async (request) => {
- const id = request.id;
- const gender = request.gender;
- const user = await User.findById(id);
- const likes = user.likes
- const dislikes = user.dislikes
- const sexuality = request.sexuality;
- console.log(gender + sexuality);
- let last_post_ids = [];
- let query = {
-    _id: { $ne: id, $nin: [...likes, ...dislikes] },
-  };
- if (sexuality === 'Heterosexual') {
- query.sex = gender === 'Masculino' ? 'Femenino' : 'Masculino';
- query.gustos = 'Heterosexual';
- } else if (sexuality === 'Bisexual') {
- query.gustos = 'Bisexual';
- } else if (sexuality === 'Gay') {
- query.sex = gender;
- query.gustos = 'Gay';
- }
- 
- console.log(query);
- const intervalFunction = async () => {
- const posts = await User.find(query);
- const post_ids = posts.map((post) => post['_id'].toString());
- if (post_ids.length != last_post_ids.length) {
- const filtered_posts = posts.filter(
- (post) => !last_post_ids.includes(post['_id'].toString())
- );
- socket.emit('message', filtered_posts);
- last_post_ids = post_ids;
- }
- };
- 
- setInterval(intervalFunction, 1000);
- });
+
  });
  
 
